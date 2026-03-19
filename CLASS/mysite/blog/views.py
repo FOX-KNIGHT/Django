a8b4ecc17774e90e1c9ab9afd7007b99d575d89b
Your Name <your_email@example.com>
2026-03-19 12:58:34 +0530

    Update mysite Django project

--- CLASS/mysite/blog/views.py
+++ CLASS/mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import CommentForm,EmailPostForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 # Create your views here.
 class PostListView(ListView):
@@ -15,15 +16,22 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-def post_list(request):
+def post_list(request,tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3) #shows 3 posts per page
     page_number = request.GET.get('page',1)
     posts = paginator.get_page(page_number)
     return render(
         request,
         'blog/post/list.html',
-        {'posts':posts}
+        {
+            'posts':posts,
+            'tag': tag
+        }
     )
 
 def post_detail(request, year, month, day, post):
