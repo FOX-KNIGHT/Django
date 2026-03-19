3c9b800035544939ba3c87744dc1e91029a23948
Your Name <your_email@example.com>
2026-03-19 12:32:19 +0530

    Update mysite Django project

--- CLASS/mysite/blog/urls.py
+++ CLASS/mysite/blog/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-
 app_name = 'blog'
-urlpatterns = [
-    #path('', views.post_list, name='post_list'),
+urlpatterns=[
+    #path('', views.post_list, name = 'post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
@@ -11,4 +10,7 @@ urlpatterns = [
         name='post_detail'
     ),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path(
+        '<int:post_id>/comment/', views.post_comment, name='post_comment'
+    ),
 ]
--- CLASS/mysite/mysite/settings.py
+++ CLASS/mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from decouple import config
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-#Email server config
 STATIC_URL = 'static/'
+
+#Email server config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
